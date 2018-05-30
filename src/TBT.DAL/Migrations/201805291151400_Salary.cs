279f3325f25d56efd15f64d5f3c28ac2ba203322
Roman <romangnus.rg@gmail.com>
2018-05-30 13:04:07 +0300

    Add migration

---
+++ src/TBT.DAL/Migrations/201805291151400_Salary.cs
@@ -0,0 +1,18 @@
+namespace TBT.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Salary : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "MonthlySalary", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Users", "MonthlySalary");
+        }
+    }
+}
